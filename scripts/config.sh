f2ab6114b8a4e587c7bcd85fee52923519a5ba2a
DanielDuP <54242914+DanielDuP@users.noreply.github.com>
2026-04-16 12:12:17 -0400

    fix: stow should no fold

--- scripts/config.sh
+++ scripts/config.sh
@@ -20,26 +20,15 @@ stow_dotfiles() {
 		".zshenv"
 		".zprofile"
 	)
-	local folders=(
-		".config/fd"
-		".config/git"
-		".config/nvim"
-	)
 	info "Removing existing config files"
 	for f in "${files[@]}"; do
 		rm -f "$HOME/$f" || true
 	done
 
-	# Create the folders to avoid symlinking folders
-	for d in "${folders[@]}"; do
-		rm -rf "${HOME:?}/$d" || true
-		mkdir -p "$HOME/$d"
-	done
-
 	# shellcheck disable=SC2155
 	local to_stow="$(find stow -maxdepth 1 -type d -mindepth 1 | awk -F "/" '{print $NF}' ORS=' ')"
 	info "Stowing: $to_stow"
-	stow -d stow --verbose 1 --target "$HOME" "$to_stow"
+	stow -d stow --no-folding --verbose 1 --target "$HOME" "$to_stow"
 
 	# set permissions
 	chmod a+x ~/.git-templates/hooks/pre-commit
